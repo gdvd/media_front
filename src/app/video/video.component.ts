7e597d4cca30c7ad785610e7a252ad506ff5a343
Gilles David <gd.noads@gmail.com>
2019-03-02 16:14:50 +0100

    List of all paths, give the list in one path, and begin of tbl video

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 @Component({
@@ -10,26 +10,74 @@ export class VideoComponent implements OnInit {
 
   paths;
   idVideo;
+  infoVideo;
+  listVideowithName;
 
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit() {
-    this.httpClient.get("http://localhost:8085/path/allkeys")
-      .subscribe(data=>{
-        this.paths=data;
+    this.infoVideo=null;
+    this.httpClient.get('http://localhost:8085/path/allkeys')
+      .subscribe(data => {
+        this.paths = data;
         console.log(data);
-      }, err=>{
+      }, err => {
         console.log(err);
       });
   }
 
   onGetOnePath(p: string) {
-    this.httpClient.get("http://localhost:8085/path/"+escape(p))
-      .subscribe(data=>{
-        this.paths=data;
+    this.httpClient.get('http://localhost:8085/path/' + escape(p))
+      .subscribe( data => {
+        this.idVideo = data;
+        console.log(data);
+        this.infoVideo=null;
+        this.listVideowithName=[];
+        for (let id of this.idVideo) {
+          let srturl = 'http://localhost:8085/videoByIdToInfo/name/' + id;
+          this.httpClient.get(srturl)
+            .subscribe(data => {
+              this.listVideowithName.push(data);
+            }, err => {
+              console.log(err);
+            });
+        };
+        console.log(this.listVideowithName)
+      }, err => {
+        console.log(err);
+      });
+  }
+  getAllInfo(id){
+    this.httpClient.get('http://localhost:8085/videoByIdToInfo/'+id+"/default")
+      .subscribe(data => {
+        this.infoVideo = data;
         console.log(data);
       }, err=>{
         console.log(err);
       });
   }
+
+  getUrlFile(i){
+    let ut = i.urlFile;
+    let us = ut[0];
+    return Object.values(us);
+  }
 }
+/*class MyObject {
+  id: string;
+  urlFile: any;
+  info: any;
+  video: any;
+  audio: any;
+  text: any;
+
+  constructor(id, name, info, video, audio, text){
+    this.id = id;
+    this.urlFile = name;
+    this.info = info;
+    this.video = video;
+    this.audio = audio;
+    this.text = text;
+  }
+}*/
