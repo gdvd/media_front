08238bccae3213bee87d8bdfa79480c85e6019b8
Gilles David <gd.noads@gmail.com>
2019-03-03 17:25:03 +0100

    Routing works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,18 @@ import { ConfigComponent } from './config/config.component';
 import { VideoComponent } from './video/video.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConfigComponent,
-    VideoComponent
+    VideoComponent,
+    ToolbarComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule, FormsModule, HttpClientModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
-
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
@@ -50,7 +49,6 @@ export class VideoComponent implements OnInit {
             });
         }
         ;
-        // console.log(this.listVideowithName);
       }, err => {
         console.log(err);
       });
@@ -73,31 +71,12 @@ export class VideoComponent implements OnInit {
     let us = ut[0];
     return Object.values(us);
   }
-
-  getAllKeys(obj) {
-    return Object.keys(obj);
-  }
-
-  getAllValues(obj) {
-    return Object.values(obj);
-  }
-
   inHMS(d) {
     let s = Math.trunc(d % 60);
     let m = Math.trunc(((d - s) / 60) % 60);
     let h = Math.trunc((d - (m * 60) - (s)) / 3600);
     return '' + h + ' h ' + m + ' mn ' + s + ' sec (' + d + 'sec)';
   }
-
-  inGMK(o) {
-    // console.log(o);
-    let oo = Math.trunc(o % 1024);
-    let k = Math.trunc((o / 1024) % 1024);
-    let m = Math.trunc(((o - k) / 1024 / 1024) % 1024);
-    let g = Math.trunc((o - (m * 1024 * 1024) - (k * 1024)) / 1024 / 1024 / 1024);
-    return '' + g + 'Go ' + m + 'Mo ' + k + 'Ko ' + oo + 'o  (' + o + ')';
-  }
-
   inGMK2(ot) {
     var res = ot;
     let units = ['o', 'Ko', 'Mo', 'Go', 'To'];
@@ -118,7 +97,6 @@ export class VideoComponent implements OnInit {
             deci = '';
           }
         }
-        // console.log('res:' + res + ' ve:' + ve + ' deci:' + deci + ' calStr:' + calStr);
         result = '' + ve + units[i] + deci + ' (' + ot + 'oct)';
       } else {
         break;
